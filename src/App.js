874a237951abe4945fc32f83eaab296a92d1ff18
Sakshi <chauhansakshi4m@gmail.com>
2023-02-11 01:30:32 +0530

    api added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import { Component } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                name: "Shibon",
-                job: 'Cyber Security Engineer',
-            },
-            {
-                name: "Nadine",
-                job: 'Project Coordinator',
-            },
-            {
-                name: "Marcia",
-                job: 'Program Assistant',
-            },
-            {
-                name: "Kendall",
-                job: 'Prgram Manager',
-            },
-        ],
+        characters: [],
     }
 
     removeCharacter = (index) => {
@@ -33,12 +17,17 @@ class App extends Component {
         })
     }
 
+    handleSubmit = (character) => {
+        this.setState({ characters: [...this.state.characters, character] })
+    }
+
     render() {
         const { characters } = this.state
 
         return (
             <div className='container'>
                 <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         )
     }
--- src/Form.js
+++ src/Form.js
@@ -18,6 +18,35 @@ class Form extends Component {
     }
 
     state = this.initialState
+
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    }
+
+    render() {
+        const { name, job } = this.state;
+
+        return (
+            <form>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={this.handleChange} />
+                <label htmlFor="job">Job</label>
+                <input
+                    type="text"
+                    name="job"
+                    id="job"
+                    value={job}
+                    onChange={this.handleChange} />
+                <input type="button" value="Submit" onClick={this.submitForm} />
+            </form>
+        )
+    }
 }
 
 export default Form
